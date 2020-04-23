67c7247f1e29e753b27752b93752e0ad006c6f2b
Mauri mohite <bhosaleshri@gmail.com>
2020-04-23 12:22:10 +0530

    merge master with newWork

--- src/main/java/com/shreeya/amazone/FactoryExprement.java
+++ src/main/java/com/shreeya/amazone/FactoryExprement.java
@@ -4,9 +4,5 @@ import org.testng.annotations.Factory;
 
 public class FactoryExprement {
 	
-	@Factory
-	public Object[] myfactory() {
-		return new Object [] {new TestFactory1("Women cloths"),new TestFactory2("men cloths")};
-	}
 
 }
